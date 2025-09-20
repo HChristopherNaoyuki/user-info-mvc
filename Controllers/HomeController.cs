74ae878f675a6954216c18d65ce83c4ebd45f943
HChristopherNaoyuki <st10462415@rcconnect.edu.za>
2025-09-20 21:09:50 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using user_info_mvc.Models;
+
+namespace user_info_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // Create an instance of the Details model
+            var userDetails = new Details();
+            return View(userDetails);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = System.Diagnostics.Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Details.cs
@@ -0,0 +1,20 @@
+﻿namespace user_info_mvc.Models
+{
+    public class Details
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public Details()
+        {
+            Name = "Yuki";
+            Surname = "H.";
+        }
+
+        public Details(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+    }
+}
